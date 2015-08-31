e407a64d27b8a748c9ed66a55c7799ea9237ce17
xfischer <xavier@fischer.ninja>
2015-09-01 00:06:43 +0200

    Category tree

--- MvcGestionAsso/Controllers/CategoriesActiviteController.cs
+++ MvcGestionAsso/Controllers/CategoriesActiviteController.cs
@@ -9,126 +9,270 @@ using System.Web;
 using System.Web.Mvc;
 using MvcGestionAsso.DataLayer;
 using MvcGestionAsso.Models;
+using TreeUtility;
+using MvcGestionAsso.ViewModels;
+using System.Data.Entity.Infrastructure;
 
 namespace MvcGestionAsso.Controllers
 {
-    public class CategoriesActiviteController : Controller
-    {
-        private ApplicationDbContext db = new ApplicationDbContext();
+	public class CategoriesActiviteController : Controller
+	{
+		private ApplicationDbContext _applicationDbContext = new ApplicationDbContext();
 
-        // GET: CategorieActivites
-        public async Task<ActionResult> Index()
-        {
-            var categoriesActivite = db.CategoriesActivite.Include(c => c.Parent);
-            return View(await categoriesActivite.ToListAsync());
-        }
+		private List<CategorieActivite> GetListOfNodes()
+		{
+			List<CategorieActivite> sourceCategories = _applicationDbContext.CategoriesActivite.ToList();
+			List<CategorieActivite> categories = new List<CategorieActivite>();
+			foreach (CategorieActivite sourceCategorie in sourceCategories)
+			{
+				CategorieActivite c = new CategorieActivite();
+				c.Id = sourceCategorie.Id;
+				c.CategorieActiviteNom = sourceCategorie.CategorieActiviteNom;
+				if (sourceCategorie.ParentId != null)
+				{
+					c.Parent = new CategorieActivite();
+					c.Parent.Id = (int)sourceCategorie.ParentId;
+				}
+				categories.Add(c);
+			}
+			return categories;
+		}
 
-        // GET: CategorieActivites/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CategorieActivite categorieActivite = await db.CategoriesActivite.FindAsync(id);
-            if (categorieActivite == null)
-            {
-                return HttpNotFound();
-            }
-            return View(categorieActivite);
-        }
+		private string EnumerateNodes(CategorieActivite parent)
+		{
+			// Init an empty string
+			string content = String.Empty;
 
-        // GET: CategorieActivites/Create
-        public ActionResult Create()
-        {
-            ViewBag.ParentId = new SelectList(db.CategoriesActivite, "Id", "CategorieActiviteNom");
-            return View();
-        }
+			// Add <li> categorie name
+			content += "<li class=\"treenode\">";
+			content += parent.CategorieActiviteNom;
+			content += String.Format("<a href=\"/CategoriesActivite/Edit/{0}\" class=\"btn btn-primary btn-xs treenodeeditbutton\">Modifier</a>", parent.Id);
+			content += String.Format("<a href=\"/CategoriesActivite/Delete/{0}\" class=\"btn btn-danger btn-xs treenodedeletebutton\">Supprimer</a>", parent.Id);
 
-        // POST: CategorieActivites/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "Id,ParentId,CategorieActiviteNom")] CategorieActivite categorieActivite)
-        {
-            if (ModelState.IsValid)
-            {
-                db.CategoriesActivite.Add(categorieActivite);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
+			// If there are no children, end the </li>
+			if (parent.Children.Count == 0)
+				content += "</li>";
+			else   // If there are children, start a <ul>
+				content += "<ul>";
 
-            ViewBag.ParentId = new SelectList(db.CategoriesActivite, "Id", "CategorieActiviteNom", categorieActivite.ParentId);
-            return View(categorieActivite);
-        }
+			// Loop one past the number of children
+			int numberOfChildren = parent.Children.Count;
+			for (int i = 0; i <= numberOfChildren; i++)
+			{
+				// If this iteration's index points to a child,
+				// call this function recursively
+				if (numberOfChildren > 0 && i < numberOfChildren)
+				{
+					CategorieActivite child = parent.Children[i];
+					content += EnumerateNodes(child);
+				}
 
-        // GET: CategorieActivites/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CategorieActivite categorieActivite = await db.CategoriesActivite.FindAsync(id);
-            if (categorieActivite == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ParentId = new SelectList(db.CategoriesActivite, "Id", "CategorieActiviteNom", categorieActivite.ParentId);
-            return View(categorieActivite);
-        }
+				// If this iteration's index points past the children, end the </ul>
+				if (numberOfChildren > 0 && i == numberOfChildren)
+					content += "</ul>";
+			}
 
-        // POST: CategorieActivites/Edit/5
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,ParentId,CategorieActiviteNom")] CategorieActivite categorieActivite)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(categorieActivite).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ParentId = new SelectList(db.CategoriesActivite, "Id", "CategorieActiviteNom", categorieActivite.ParentId);
-            return View(categorieActivite);
-        }
+			// Return the content
+			return content;
+		}
 
-        // GET: CategorieActivites/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CategorieActivite categorieActivite = await db.CategoriesActivite.FindAsync(id);
-            if (categorieActivite == null)
-            {
-                return HttpNotFound();
-            }
-            return View(categorieActivite);
-        }
 
-        // POST: CategorieActivites/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
-            CategorieActivite categorieActivite = await db.CategoriesActivite.FindAsync(id);
-            db.CategoriesActivite.Remove(categorieActivite);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
+		private void ValidateParentsAreParentless(CategorieActivite categorie)
+		{
+			// There is no parent
+			if (categorie.ParentId == null)
+				return;
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+			// The parent has a parent
+			CategorieActivite parentCategory = _applicationDbContext.CategoriesActivite.Find(categorie.ParentId);
+			if (parentCategory.ParentId != null)
+				throw new InvalidOperationException("Seuls deux niveaux hiérarchiques sont autorisés.");
+
+			// The parent does NOT have a parent, but the categorie being nested has children
+			int numberOfChildren = _applicationDbContext.CategoriesActivite.Count(c => c.ParentId== categorie.Id);
+			if (numberOfChildren > 0)
+				throw new InvalidOperationException("Seuls deux niveaux hiérarchiques sont autorisés.");
+		}
+
+
+		private SelectList PopulateParentCategorySelectList(int? id)
+		{
+			SelectList selectList;
+
+			if (id == null)
+				selectList = new SelectList(
+						_applicationDbContext
+						.CategoriesActivite
+						.Where(c => c.ParentId == null), "Id", "CategorieActiviteNom");
+			else if (_applicationDbContext.CategoriesActivite.Count(c => c.ParentId == id) == 0)
+				selectList = new SelectList(
+						_applicationDbContext
+						.CategoriesActivite
+						.Where(c => c.ParentId == null && c.Id != id), "Id", "CategorieActiviteNom");
+			else
+				selectList = new SelectList(
+						_applicationDbContext
+						.CategoriesActivite
+						.Where(c => false), "Id", "CategorieActiviteNom");
+
+			return selectList;
+		}
+
+		public async Task<ActionResult> Index()
+		{
+			// Start the outermost list
+			string fullString = "<ul>";
+
+			IList<CategorieActivite> listOfNodes = GetListOfNodes();
+			IList<CategorieActivite> topLevelCategories = TreeHelper.ConvertToForest(listOfNodes);
+
+			foreach (var category in topLevelCategories)
+				fullString += EnumerateNodes(category);
+
+			// End the outermost list
+			fullString += "</ul>";
+
+			return View((object)fullString);
+		}
+
+		public ActionResult Create()
+		{
+			ViewBag.ParentCategoryIdSelectList = PopulateParentCategorySelectList(null);
+			return View();
+		}
+
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create([Bind(Include = "Id,ParentId,CategorieActiviteNom")] CategorieActivite category)
+		{
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					ValidateParentsAreParentless(category);
+				}
+				catch (Exception ex)
+				{
+					ModelState.AddModelError("", ex.Message);
+					ViewBag.ParentCategoryIdSelectList = PopulateParentCategorySelectList(null);
+					return View(category);
+				}
+
+
+				_applicationDbContext.CategoriesActivite.Add(category);
+				await _applicationDbContext.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+
+			ViewBag.ParentCategoryIdSelectList = PopulateParentCategorySelectList(null);
+			return View(category);
+		}
+
+
+		public async Task<ActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			CategorieActivite category = await _applicationDbContext.CategoriesActivite.FindAsync(id);
+			if (category == null)
+			{
+				return HttpNotFound();
+			}
+
+			// Wind-up a Category viewmodel
+			CategorieActiviteViewModel categoryViewModel = new CategorieActiviteViewModel();
+			categoryViewModel.Id = category.Id;
+			categoryViewModel.ParentId = category.ParentId;
+			categoryViewModel.CategorieActiviteNom = category.CategorieActiviteNom;
+			
+			ViewBag.ParentCategoryIdSelectList = PopulateParentCategorySelectList(categoryViewModel.Id);
+			return View(categoryViewModel);
+		}
+
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Edit([Bind(Include = "Id,ParentId,CategorieActiviteNom")] CategorieActiviteViewModel categoryViewModel)
+		{
+			if (ModelState.IsValid)
+			{
+				// Unwind back to a Category
+				CategorieActivite editedCategory = new CategorieActivite();
+
+				try
+				{
+					editedCategory.Id = categoryViewModel.Id;
+					editedCategory.ParentId = categoryViewModel.ParentId;
+					editedCategory.CategorieActiviteNom = categoryViewModel.CategorieActiviteNom;
+					ValidateParentsAreParentless(editedCategory);
+				}
+				catch (Exception ex)
+				{
+					ModelState.AddModelError("", ex.Message);
+					ViewBag.ParentCategoryIdSelectList = PopulateParentCategorySelectList(categoryViewModel.Id);
+					return View("Edit", categoryViewModel);
+				}
+
+
+				_applicationDbContext.Entry(editedCategory).State = EntityState.Modified;
+				await _applicationDbContext.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			ViewBag.ParentCategoryIdSelectList = PopulateParentCategorySelectList(categoryViewModel.Id);
+			return View(categoryViewModel);
+		}
+
+
+		public async Task<ActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			CategorieActivite category = await _applicationDbContext.CategoriesActivite.FindAsync(id);
+			if (category == null)
+			{
+				return HttpNotFound();
+			}
+			return View(category);
+		}
+
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> DeleteConfirmed(int id)
+		{
+			CategorieActivite category = await _applicationDbContext.CategoriesActivite.FindAsync(id);
+
+			try
+			{
+				_applicationDbContext.CategoriesActivite.Remove(category);
+				await _applicationDbContext.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			catch (DbUpdateException)
+			{
+				ModelState.AddModelError("", "Vous ne pouvez pas supprimer une catégorie qui a des sous-catégories.");
+			}
+			catch (Exception ex)
+			{
+				ModelState.AddModelError("", ex.Message);
+			}
+
+			return View("Delete", category);
+		}
+
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				_applicationDbContext.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
---
+++ MvcGestionAsso/ViewModels/CategorieActiviteViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MvcGestionAsso.Models;
+
+namespace MvcGestionAsso.ViewModels
+{
+	public class CategorieActiviteViewModel
+    {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Catégorie parente")]
+        public int? ParentId { get; set; }
+
+        [Required(ErrorMessage = "Vous devez saisir un nom de catégorie.")]
+        [StringLength(50, ErrorMessage = "Le nom des catégories doivent comporter moins de 50 caractères.")]
+        [Display(Name = "Catégorie")]
+        public string CategorieActiviteNom { get; set; }
+        public virtual List<Activite> Activites { get; set; }
+    }
+}
