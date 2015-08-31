465636f56f4cb70e0d4e2f6fff619a503f76800e
Xavier Fischer <xavier@fischer.ninja>
2015-09-01 00:50:42 +0200

    Categorie translation fixes

--- MvcGestionAsso/Models/CategorieActivite.cs
+++ MvcGestionAsso/Models/CategorieActivite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using TreeUtility;
@@ -10,13 +11,28 @@ namespace MvcGestionAsso.Models
 	{
 		public int Id { get; set; }
 
-		public int? ParentId { get; set; }
+		private int? _parentId;
+		[Display(Name = "Catégorie parente")]
+		public int? ParentId
+		{
+			get { return _parentId; }
+			set
+			{
+				if (Id == value)
+					throw new InvalidOperationException("Une catégorie ne peut être son propre parent.");
 
+				_parentId = value;
+			}
+		}
+
+		[Required(ErrorMessage = "Vous devez saisir un nom de catégorie.")]
+		[StringLength(50, ErrorMessage = "Le nom de la catégorie doit comporter moins de 50 caractères.")]
+		[Display(Name = "Catégorie")]    
 		public string CategorieActiviteNom { get; set; }
 
 		public virtual List<Activite> Activites { get; set; }
 
-		public CategorieActivite Parent { get; set; }
+		public virtual CategorieActivite Parent { get; set; }
 
 		public IList<CategorieActivite> Children { get; set; }
 	}
