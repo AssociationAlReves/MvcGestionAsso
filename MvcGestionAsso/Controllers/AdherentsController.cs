7b932c8faa57762de17894837e7f5a1ebc45e136
xfischer <xavier@fischer.ninja>
2015-09-02 16:21:27 +0200

    Sort on Adherents controller

--- MvcGestionAsso/Controllers/AdherentsController.cs
+++ MvcGestionAsso/Controllers/AdherentsController.cs
@@ -12,119 +12,217 @@ using MvcGestionAsso.Models;
 
 namespace MvcGestionAsso.Controllers
 {
-    public class AdherentsController : Controller
-    {
-        private ApplicationDbContext db = new ApplicationDbContext();
+	public class AdherentsController : Controller
+	{
+		private ApplicationDbContext _applicationDbContext = new ApplicationDbContext();
 
-        // GET: Adherents
-        public async Task<ActionResult> Index()
-        {
-            return View(await db.Adherents.ToListAsync());
-        }
+		// GET: Adherents
+		public async Task<ActionResult> Index(string sort)
+		{
+			ViewBag.NomSort = ComputeSort("nom", sort, true);
+			ViewBag.PrenomSort = ComputeSort("prenom", sort);
+			ViewBag.FamilleSort = ComputeSort("famille", sort);
+			ViewBag.DateCreationSort = ComputeSort("datecreation", sort);
+			ViewBag.DateModifSort = ComputeSort("datemodif", sort);
+			ViewBag.StatutSort = ComputeSort("statut", sort);
 
-        // GET: Adherents/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Adherent adherent = await db.Adherents.FindAsync(id);
-            if (adherent == null)
-            {
-                return HttpNotFound();
-            }
-            return View(adherent);
-        }
+			IQueryable<Adherent> adherents = _applicationDbContext.Adherents;
 
-        // GET: Adherents/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+			#region Sorting
+			switch (sort)
+			{
+				case "nom_desc":
+					adherents = adherents
+												.OrderByDescending(a => a.AdherentNom)
+												.ThenByDescending(a => a.DateCreation);
+					break;
 
-        // POST: Adherents/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "AdherentId,AdherentNom,AdherentPrenom,Famille,Notes,EMail,Telephone,Adresse,Adresse2,CodePostal,Ville,DateCreation,DateModification,DateResiliation,Statut,CertificatMedical")] Adherent adherent)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Adherents.Add(adherent);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
+				case "prenom":
+					adherents = adherents
+												.OrderBy(a => a.AdherentPrenom);
+					break;
 
-            return View(adherent);
-        }
+				case "prenom_desc":
+					adherents = adherents
+												.OrderByDescending(a => a.AdherentPrenom);
+					break;
 
-        // GET: Adherents/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Adherent adherent = await db.Adherents.FindAsync(id);
-            if (adherent == null)
-            {
-                return HttpNotFound();
-            }
-            return View(adherent);
-        }
+				case "famille":
+					adherents = adherents
+												.OrderBy(a => a.Famille);
+					break;
 
-        // POST: Adherents/Edit/5
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "AdherentId,AdherentNom,AdherentPrenom,Famille,Notes,EMail,Telephone,Adresse,Adresse2,CodePostal,Ville,DateCreation,DateModification,DateResiliation,Statut,CertificatMedical")] Adherent adherent)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(adherent).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            
-					return View(adherent);
-        }
+				case "famille_desc":
+					adherents = adherents
+												.OrderByDescending(a => a.Famille);
+					break;
 
-        // GET: Adherents/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Adherent adherent = await db.Adherents.FindAsync(id);
-            if (adherent == null)
-            {
-                return HttpNotFound();
-            }
-            return View(adherent);
-        }
+				case "datecreation":
+					adherents = adherents
+												.OrderBy(a => a.DateCreation);
+					break;
 
-        // POST: Adherents/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
-            Adherent adherent = await db.Adherents.FindAsync(id);
-            db.Adherents.Remove(adherent);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
+				case "datecreation_desc":
+					adherents = adherents
+												.OrderByDescending(a => a.DateCreation);
+					break;
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+				case "datemodif":
+					adherents = adherents
+												.OrderBy(a => a.DateModification);
+					break;
+
+				case "datemodif_desc":
+					adherents = adherents
+												.OrderByDescending(a => a.DateModification);
+					break;
+
+				case "statut":
+					adherents = adherents
+												.OrderBy(a => a.Statut);
+					break;
+
+				case "statut_desc":
+					adherents = adherents
+												.OrderByDescending(a => a.Statut);
+					break;
+
+				default:
+					adherents = adherents
+												.OrderBy(a => a.AdherentNom)
+												.ThenByDescending(a => a.DateCreation);
+					break;
+			}
+			#endregion
+
+			return View(await adherents.ToListAsync());
+		}
+
+		private string ComputeSort(string fieldName, string inputSort, bool isDefault = false, bool isDefaultSortAscending = true)
+		{
+			string outputSort = null;
+			if (isDefault)
+			{
+				if (isDefaultSortAscending)
+				{
+					outputSort = string.IsNullOrEmpty(inputSort) ? fieldName + "_desc" : string.Empty;
+				}
+				else
+				{
+					outputSort = string.IsNullOrEmpty(inputSort) ? fieldName : string.Empty; 
+				}
+			}
+			else
+			{
+				outputSort = inputSort == fieldName ? fieldName + "_desc" : fieldName;
+			}
+
+			return outputSort;
+		}
+
+		// GET: Adherents/Details/5
+		public async Task<ActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Adherent adherent = await _applicationDbContext.Adherents.FindAsync(id);
+			if (adherent == null)
+			{
+				return HttpNotFound();
+			}
+			return View(adherent);
+		}
+
+		// GET: Adherents/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Adherents/Create
+		// Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+		// plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create([Bind(Include = "AdherentId,AdherentNom,AdherentPrenom,Famille,Notes,EMail,Telephone,Adresse,Adresse2,CodePostal,Ville,DateCreation,DateModification,DateResiliation,Statut,CertificatMedical")] Adherent adherent)
+		{
+			if (ModelState.IsValid)
+			{
+				_applicationDbContext.Adherents.Add(adherent);
+				await _applicationDbContext.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+
+			return View(adherent);
+		}
+
+		// GET: Adherents/Edit/5
+		public async Task<ActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Adherent adherent = await _applicationDbContext.Adherents.FindAsync(id);
+			if (adherent == null)
+			{
+				return HttpNotFound();
+			}
+			return View(adherent);
+		}
+
+		// POST: Adherents/Edit/5
+		// Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+		// plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Edit([Bind(Include = "AdherentId,AdherentNom,AdherentPrenom,Famille,Notes,EMail,Telephone,Adresse,Adresse2,CodePostal,Ville,DateCreation,DateModification,DateResiliation,Statut,CertificatMedical")] Adherent adherent)
+		{
+			if (ModelState.IsValid)
+			{
+				_applicationDbContext.Entry(adherent).State = EntityState.Modified;
+				await _applicationDbContext.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+
+			return View(adherent);
+		}
+
+		// GET: Adherents/Delete/5
+		public async Task<ActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Adherent adherent = await _applicationDbContext.Adherents.FindAsync(id);
+			if (adherent == null)
+			{
+				return HttpNotFound();
+			}
+			return View(adherent);
+		}
+
+		// POST: Adherents/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> DeleteConfirmed(int id)
+		{
+			Adherent adherent = await _applicationDbContext.Adherents.FindAsync(id);
+			_applicationDbContext.Adherents.Remove(adherent);
+			await _applicationDbContext.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				_applicationDbContext.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
